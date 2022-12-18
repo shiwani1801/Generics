08cfa0ffa83e3c7f3c0bfb0366880f261e68c12b
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2022-12-18 07:20:20 -0800

    Update Generics.java

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -19,33 +19,4 @@ public class Generics <T extends Comparable<T>> {
         return null;
     }
 
-    public <T extends Comparable> Object maxMethod(T firstValue,T secondValue, T thirdValue) {
-        T maxValue;
-
-         if ((thirdValue.compareTo(secondValue) > 0) && (thirdValue.compareTo(firstValue)  > 0)) {
-            System.out.println(thirdValue + " greater than " + secondValue + " and " + firstValue );
-            maxValue=thirdValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue);
-        }else  if ((secondValue.compareTo(firstValue) > 0) && (secondValue.compareTo(thirdValue)  > 0)) {
-            System.out.println(secondValue + " greater than " + firstValue + " and" + thirdValue);
-            maxValue=secondValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue);
-        }else  if ((firstValue.compareTo(secondValue) > 0) && (firstValue.compareTo(thirdValue)  > 0)) {
-            System.out.println(firstValue + " greater than " + secondValue + " and " + thirdValue );
-            maxValue=firstValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue);
-        }
-        return null;
-    }
-
-    public  <T extends Comparable> void printMax(T maxValue, T firstValue, T secondValue, T thirdValue) {
-        System.out.println("\nPrintMax method.............");
-        System.out.println("1st value = "+firstValue);
-        System.out.println("2nd value = "+secondValue);
-        System.out.println("3rd value = "+thirdValue);
-
-        System.out.println("\nMaximum value = "+maxValue);
-    }
-}
-
-
+   
