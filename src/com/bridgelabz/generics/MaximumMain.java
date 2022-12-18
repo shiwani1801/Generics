9b5d5e83199d7dc912d46f8015e7b9e7b9b91746
shiwani <shiwanipaunikar1@gmail.com>
2022-12-18 15:26:47 -0800

    [shiwani]find maximum integer program added

---
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.generics;
+
+public class MaximumMain {
+    public static void main(String[] args) {
+        findingMaxInteger();
+    }
+
+    private static void findingMaxInteger() {
+        System.out.println("\n Finding max integer.........");
+        Integer num1=10,num2=20,num3=100;
+        if((num3.compareTo(num2)>0)&&(num3.compareTo(num1)>0)){
+            System.out.println(num3 + "is greater than" + num2 + "and" + num1);
+        }
+        else if((num2.compareTo(num1)>0)&&(num2.compareTo(num3)>0)){
+            System.out.println(num2 + " is greater than" + num1 + "and" + num3);
+        }
+        else if((num1.compareTo(num2)>0)&&(num1.compareTo(num3)>0)){
+            System.out.println(num1 +"is greater than" + num2 + "and" + num3);
+        }
+    }
+}
