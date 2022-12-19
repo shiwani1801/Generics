c7a8dd64b0bfad14d7dc5bfcdbcc654f14a0a262
shiwani <shiwanipaunikar1@gmail.com>
2022-12-18 20:21:24 -0800

    [shiwani]printMax generic method

--- src/com/bridgelabz/generics/MaximumMain.java
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -2,35 +2,29 @@ package com.bridgelabz.generics;
 
 public class MaximumMain<T> {
     public static void main(String args[]) {
-        int choice=3;
-        switch (choice){
-            case 1:
-                findingMaxInteger();
-                break;
-            case 2:
-                findingMaxFloat();
-                break;
-            case 3:
-                findingMaxString();
-        }
+
+        findingMaxInteger();
+        findingMaxFloat();
+        findingMaxString();
 
     }
     static Generics<Integer> object = new Generics<>();
 
     public static void findingMaxInteger() {
         System.out.println("\ninteger Sorted in ascending order.....................");
-        //object number1 = 10, number2 = 400, number3 = 200 ,number4 =300;
-        object.compare(10, 400, 200,300);
+        //object number1 = 10, number2 = 30, number3 = 200;
+        object.compare(10, 300, 20,100);
     }
 
     public static void findingMaxFloat() {
         System.out.println("\nFloat Sorted in ascending order.....................");
-        Float number1 = 4.4f;
-        Float number2 = 4.8f;
-        Float number3 = 3.8f;
-        Float number4 = 9.6f;
-        object.compare(number1, number2, number3,number4);
+
+        object.compare(2.2,4.5,5.7,1.1);
+
     }
+
+
+
     public static void findingMaxString() {
         System.out.println("\nSorted in ascending order.....................");
         String firstValue = "banana", secondValue = "watermelon", thirdValue = "apple",fourthValue="plum";
