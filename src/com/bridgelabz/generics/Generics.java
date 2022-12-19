164d44ece6f31b0274d892b58a05f3672ad7ab9b
shiwani <shiwanipaunikar1@gmail.com>
2022-12-18 18:51:34 -0800

    [shiwani]Generic method to find maximum element

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -7,47 +7,42 @@ import java.util.Collections;
 public class Generics <T extends Comparable<T>> {
 
 
-    public <T extends Comparable> Object compare(T firstValue, T secondValue, T thirdValue,T fourthValue) {
+    public <T extends Comparable> Object compare(T firstValue, T secondValue, T thirdValue) {
         ArrayList <T>arrayList=new ArrayList<T>();
         arrayList.add(firstValue);
         arrayList.add(secondValue);
         arrayList.add(thirdValue);
-        arrayList.add(fourthValue);
         System.out.println(arrayList);
         Collections.sort(arrayList);
         System.out.println("sorted list : "+arrayList);
-        maxMethod(firstValue,secondValue,thirdValue,fourthValue);
+        maxMethod(firstValue,secondValue,thirdValue);
         return null;
     }
 
-    public <T extends Comparable> Object maxMethod(T firstValue,T secondValue, T thirdValue,T fourthValue) {
+    public <T extends Comparable> Object maxMethod(T firstValue,T secondValue, T thirdValue) {
         T maxValue;
-        if ((fourthValue.compareTo(secondValue) > 0) && (fourthValue.compareTo(firstValue) > 0) && (fourthValue.compareTo(fourthValue) > 0)) {
-            System.out.println(fourthValue + " greater than " + secondValue + " , " + firstValue + " and " + thirdValue);
-            maxValue=fourthValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue,fourthValue);
-        }else if ((thirdValue.compareTo(secondValue) > 0) && (thirdValue.compareTo(firstValue) > 0) && (thirdValue.compareTo(fourthValue) > 0)) {
-            System.out.println(thirdValue + " greater than " + secondValue + " , " + firstValue + " and " + fourthValue);
+
+         if ((thirdValue.compareTo(secondValue) > 0) && (thirdValue.compareTo(firstValue)  > 0)) {
+            System.out.println(thirdValue + " greater than " + secondValue + " and " + firstValue );
             maxValue=thirdValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue,fourthValue);
-        }else  if ((secondValue.compareTo(firstValue) > 0) && (secondValue.compareTo(thirdValue) > 0) && (secondValue.compareTo(fourthValue) > 0)) {
-            System.out.println(secondValue + " greater than " + firstValue + " , " + thirdValue+ " and " + fourthValue);
+            printMax(maxValue,firstValue,secondValue,thirdValue);
+        }else  if ((secondValue.compareTo(firstValue) > 0) && (secondValue.compareTo(thirdValue)  > 0)) {
+            System.out.println(secondValue + " greater than " + firstValue + " and" + thirdValue);
             maxValue=secondValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue,fourthValue);
-        }else  if ((firstValue.compareTo(secondValue) > 0) && (firstValue.compareTo(thirdValue) > 0) && (firstValue.compareTo(fourthValue) > 0)) {
-            System.out.println(firstValue + " greater than " + secondValue + " , " + thirdValue + " and " + fourthValue);
+            printMax(maxValue,firstValue,secondValue,thirdValue);
+        }else  if ((firstValue.compareTo(secondValue) > 0) && (firstValue.compareTo(thirdValue)  > 0)) {
+            System.out.println(firstValue + " greater than " + secondValue + " and " + thirdValue );
             maxValue=firstValue;
-            printMax(maxValue,firstValue,secondValue,thirdValue,fourthValue);
+            printMax(maxValue,firstValue,secondValue,thirdValue);
         }
         return null;
     }
 
-    public  <T extends Comparable> void printMax(T maxValue, T firstValue, T secondValue, T thirdValue,T fourthValue) {
+    public  <T extends Comparable> void printMax(T maxValue, T firstValue, T secondValue, T thirdValue) {
         System.out.println("\nPrintMax method.............");
         System.out.println("1st value = "+firstValue);
         System.out.println("2nd value = "+secondValue);
         System.out.println("3rd value = "+thirdValue);
-        System.out.println("4th value = "+fourthValue);
 
         System.out.println("\nMaximum value = "+maxValue);
     }
--- src/com/bridgelabz/generics/MaximumMain.java
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -12,19 +12,19 @@ public class MaximumMain<T> {
 
     public static void findingMaxInteger() {
         System.out.println("\ninteger Sorted in ascending order.....................");
-        //object number1 = 10, number2 = 30, number3 = 200,number4=300;
-        object.compare(10,30,300,100);
+        //object number1 = 10, number2 = 30, number3 = 300;
+        object.compare(10,30,300);
     }
 
     public static void findingMaxFloat() {
         System.out.println("\nFloat Sorted in ascending order.....................");
 
-        object.compare(1.1, 2.2, 4.4,5.6);
+        object.compare(1.1, 2.2, 4.4);
     }
     public static void findingMaxString() {
         System.out.println("\nSorted in ascending order.....................");
-        String firstValue = "banana", secondValue = "watermelon", thirdValue = "apple",fourthValue="plum";
-        object.compare(firstValue, secondValue, thirdValue,fourthValue);
+        String firstValue = "apple", secondValue = "peach", thirdValue = "banana";
+        object.compare(firstValue, secondValue, thirdValue);
     }
 
 }
