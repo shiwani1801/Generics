fd6c026ea5da82d3174237400e0887e113676b04
shiwani <shiwanipaunikar1@gmail.com>
2022-12-18 19:29:55 -0800

    [shiwani]extend max method to store more than three element using sorting

--- src/com/bridgelabz/generics/MaximumMain.java
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -2,29 +2,39 @@ package com.bridgelabz.generics;
 
 public class MaximumMain<T> {
     public static void main(String args[]) {
-
-        findingMaxInteger();
-        findingMaxFloat();
-        findingMaxString();
+        int choice=3;
+        switch (choice){
+            case 1:
+                findingMaxInteger();
+                break;
+            case 2:
+                findingMaxFloat();
+                break;
+            case 3:
+                findingMaxString();
+        }
 
     }
     static Generics<Integer> object = new Generics<>();
 
     public static void findingMaxInteger() {
         System.out.println("\ninteger Sorted in ascending order.....................");
-        //object number1 = 10, number2 = 30, number3 = 300;
-        object.compare(10,30,300);
+        //object number1 = 10, number2 = 400, number3 = 200 ,number4 =300;
+        object.compare(10, 400, 200,300);
     }
 
     public static void findingMaxFloat() {
         System.out.println("\nFloat Sorted in ascending order.....................");
-
-        object.compare(1.1, 2.2, 4.4);
+        Float number1 = 4.4f;
+        Float number2 = 4.8f;
+        Float number3 = 3.8f;
+        Float number4 = 9.6f;
+        object.compare(number1, number2, number3,number4);
     }
     public static void findingMaxString() {
         System.out.println("\nSorted in ascending order.....................");
-        String firstValue = "apple", secondValue = "peach", thirdValue = "banana";
-        object.compare(firstValue, secondValue, thirdValue);
+        String firstValue = "banana", secondValue = "watermelon", thirdValue = "apple",fourthValue="plum";
+        object.compare(firstValue, secondValue, thirdValue,fourthValue);
     }
 
 }
